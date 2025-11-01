e92d8856dafc646959fba1a57311a27063d3cdd5
MARCOS E. VARGAS HERNANDEZ <mevargash@edu.uia.ac.cr>
2025-10-31 21:27:24 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROYECTO-FRONT-END',
+  webDir: 'www'
+};
+
+export default config;
